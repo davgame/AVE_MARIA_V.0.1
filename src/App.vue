26639d8cefe753674b2956416d22a65d9fc97790
davgame <davshorvoglyan@rambler.ru>
2024-04-28 20:07:22 +0300

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
     <router-view/>
-    <Home/>
 </template>
 
 <script setup>
-import Home from './views/Home.vue';
 import { RouterView} from 'vue-router';
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
