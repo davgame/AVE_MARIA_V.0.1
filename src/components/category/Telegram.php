f48f279bbe0eb9a258698dbf8823d9fcd892d77a
davgame <davshorvoglyan@rambler.ru>
2024-04-30 01:15:48 +0300

    new

--- src/components/category/Telegram.php
+++ src/components/category/Telegram.php
@@ -2,21 +2,25 @@
 
 /* https://api.telegram.org/bot6848194800:AAE0HEbU3DWCL9fvBbr5TdlP-mQWNzqctCQ/getUpdates,
 где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
-
-$name = $_POST['name'];
-$surname=$_POST['surname'];
-$category=$_POST['category'];
-$email = $_POST['email'];
-$phone_input = $_POST['phone_input'];
 $token = "6848194800:AAE0HEbU3DWCL9fvBbr5TdlP-mQWNzqctCQ";
 $chat_id = "-4140320163";
-$arr = array(
-  'Имя:' => $name,
-  'Фамилия:'=>$surname,
-  'Категория обучения:'=>$category,
-  'Email' => $email,
-  'Номер телефона:' => $phone_input
-);
+
+//Определяем переменные для передачи данных из нашей формы
+if ($_POST['act'] == 'order') {
+  $name = ($_POST['name']);
+  $surname=($_POST['surname']);
+  $category=($_POST['category']);
+  $email = ($_POST['email']);
+  $phone_input = ($_POST['phone_input']);
+
+  $arr = array(
+    'Имя:' => $name,
+    'Фамилия:'=>$surname,
+    'Категория обучения:'=>$category,
+    'Email' => $email,
+    'Номер телефона:' => $phone_input
+  );
+}
 
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
