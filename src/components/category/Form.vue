019bcd2d91fd1eaf2005b90cde3e4ff65a963d36
davgame <davshorvoglyan@rambler.ru>
2024-04-30 00:09:37 +0300

    new

--- src/components/category/Form.vue
+++ src/components/category/Form.vue
@@ -19,7 +19,7 @@
             </div>
             <!-- Modal body -->
             <div class="p-4 md:p-5">
-                <form class="space-y-4" action="Telegram.php" method="POST">
+                <form class="space-y-4" action="Telegram.php" method="post">
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">Имя</label>
                         <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#141414] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Александр" required />
