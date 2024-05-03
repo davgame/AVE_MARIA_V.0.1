244dc01a4522f95b09403bd81bf46e9387cd9e25
davgame <davshorvoglyan@rambler.ru>
2024-05-03 10:56:03 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
     <router-view/>
+    <SpeedInsights/>
 </template>
 
 <script setup>
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 import { RouterView} from 'vue-router';
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
