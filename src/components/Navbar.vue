e3b42125c223c8cfa6d9e436e9bd89792b1d7cab
davgame <davshorvoglyan@rambler.ru>
2024-05-09 11:41:25 +0300

    New

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,10 +20,9 @@
         
         <button data-modal-target="select-modal" data-modal-toggle="select-modal" type="button" class="block text-white bg-[#000000] hover:bg-[#4286F7] hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-[#4286F7] dark:text-white dark:hover:bg-white dark:hover:text-[#4286F7] focus:outline-none dark:focus:ring-white">Краснодар</button>
         
-        <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+        <button @click="burgerIsOpened = !burgerIsOpened" data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
-            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <box-icon @onclick="onToggleMenu(select-modal)" :name="burgerIsOpened ? 'x' : 'menu'" class="cursor-pointer md:hidden"></box-icon>
         </button>
     </div>
     <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
@@ -82,6 +81,12 @@ import { useDark, useToggle } from '@vueuse/core'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+
+const burgerIsOpened = ref(false)
+
+function onToggleMenu(e){
+            e.name = e.name === 'menu' ? 'x' : 'menu'}
+
 </script>
 
 
