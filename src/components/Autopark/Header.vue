8a836e3442ab75da58e6243598d811aa6996313f
davgame <davshorvoglyan@rambler.ru>
2024-05-10 15:38:24 +0300

    New

--- src/components/Autopark/Header.vue
+++ src/components/Autopark/Header.vue
@@ -1,3 +1,3 @@
 <template>
-    <p class="lg:mt-32 mt-28 text-left mb-14 lg:mb-24 text-6xl font-bold leading-none tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-8xl dark:text-white"><span class="text-[#4286F7] dark:text-blue-500">АВТОПАРК</span></p>
+    <p class="lg:mt-32 mt-28 text-left mb-14 lg:mb-24 text-5xl font-bold leading-none tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-8xl dark:text-white font-Rubik"><span class="text-[#4286F7] dark:text-blue-500">АВТОПАРК</span></p>
 </template>
--- src/components/instructor/Header.vue
+++ src/components/instructor/Header.vue
@@ -1,4 +1,4 @@
 <template>
-    <p class=" font-bold lg:mt-32 text-4xl text-left mt-28 mb-8 lg:mb-24 tracking-tight text-gray-900 dark:text-white"><span class="text-[#4286F7] dark:text-[#4286F7] md:text-7xl lg:text-8xl">ИНСТРУКТОРЫ</span></p>
+    <p class=" font-bold lg:mt-32 text-4xl text-left mt-28 mb-8 lg:mb-24 tracking-tight text-gray-900 dark:text-white font-Rubik"><span class="text-[#4286F7] dark:text-[#4286F7] md:text-7xl lg:text-8xl">ИНСТРУКТОРЫ</span></p>
 </template>
 
